68be75dce2cc477801d5f98a3511c1add501e0f6
Andreas Schaefer <hea76404@adobe.com>
2022-06-01 13:45:41 -0700

    Some Sidekick Configuration fixes

--- tools/sidekick/config.js
+++ tools/sidekick/config.js
@@ -5,7 +5,15 @@
   plugins: [
     {
       id: 'Inserts',
-      button: 'Insert Into',
+      button: {
+        text: 'Insert Into',
+        action: function () {
+          alert('Plugin Button Clicked');
+        },
+        isPressed: function () {
+          alert('Plugin Button Pressed')
+        }
+      },
       callback: function (sidekick, plugin) {
         alert('Callback called, sidekick: ' + sidekick + ', plugin: ' + plugin);
       }
