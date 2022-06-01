324e3f039caf6d2f155f0f86194167dd1dd8fc78
Andreas Schaefer <hea76404@adobe.com>
2022-06-01 14:51:25 -0700

    Adding alert for debugging

--- tools/sidekick/config.js
+++ tools/sidekick/config.js
@@ -8,14 +8,14 @@
       button: {
         text: 'Insert Into',
         action: function () {
-          // alert('Plugin Button Clicked');
+          alert('Plugin Button Clicked');
         },
         isPressed: function () {
-          // alert('Plugin Button Pressed')
+          alert('Plugin Button Pressed')
         }
       },
       callback: function (sidekick, plugin) {
-        // alert('Callback called, sidekick: ' + sidekick + ', plugin: ' + plugin);
+        alert('Callback called, sidekick: ' + sidekick + ', plugin: ' + plugin);
       }
     }
   ]
