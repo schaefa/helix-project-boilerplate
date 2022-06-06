505b98878df4d651bf832fa4d73e30a3f9fb499d
Andreas Schaefer <hea76404@adobe.com>
2022-06-06 10:44:47 -0700

    Added the extraction of the Document Id

--- tools/sidekick/hw-plugin.js
+++ tools/sidekick/hw-plugin.js
@@ -1,7 +1,11 @@
+const CLIENT_ID = '<YOUR_CLIENT_ID>';
+const API_KEY = '<YOUR_API_KEY>';
+
 export default class HwPlugin {
 
   mySidekick
   myPlugin
+  gapiInited
 
   constructor(sidekick, plugin) {
     this.mySidekick = sidekick
@@ -10,15 +14,43 @@ export default class HwPlugin {
 
   insertButtonClicked() {
     console.log('Insert Button Click called, sidekick: ' + this.mySidekick + ', plugin: ' + this.myPlugin);
+    let url = document.location.href
+    let startingPos = url.search('\/d\/');
+    let endingPos = url.search('\/edit');
+    console.log('Url: ' + url + ', staring pos: ' + startingPos + ', ending pos: ' + endingPos);
+    if(startingPos > 0 && endingPos > startingPos) {
+      let documentId = url.sub(startingPos + 3, endingPos);
+      console.log('Document Id: ' + documentId);
+    }
+
     // for(let key in this.myPlugin) {
     //   console.log('Plugin, key: ' + key + ', value: ' + this.myPlugin[key])
     // }
-    chrome.identity.getAuthToken(
-      {
-        callback: function (token, grantedScope) {
-          console.log('Auth Token: ' + token + ', granted scope: ' + grantedScope)
-        }
-      }
-    )
+    // chrome.identity.getAuthToken(
+    //   {
+    //     callback: function (token, grantedScope) {
+    //       console.log('Auth Token: ' + token + ', granted scope: ' + grantedScope)
+    //     }
+    //   }
+    // )
+  }
+
+  /**
+   * Callback after api.js is loaded.
+   */
+  gapiLoaded() {
+    gapi.load('client', intializeGapiClient);
+  }
+
+  /**
+   * Callback after the API client is loaded. Loads the
+   * discovery doc to initialize the API.
+   */
+  async intializeGapiClient() {
+    await gapi.client.init({
+      apiKey: API_KEY,
+      discoveryDocs: [DISCOVERY_DOC],
+    });
+    this.gapiInited = true;
   }
 }
